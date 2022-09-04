efb11b4f0ce5fb260721542ae2454f5f1a70c0bb
Yaroslav <yaroslav.garashchenko@gmail.com>
2022-09-04 14:45:42 +0300

    Update script_special_var.sh

--- script_special_var.sh
+++ script_special_var.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #Tasks for topic of special variables
-#Nane of file-script
-echo "file: " $0
+#Name of file-script
+echo "Executable file: " $0
 
 #prints all arguments
-echo "prints all arguments:" $@
+echo "Prints all arguments:" $@
 
 #number of arguments passed to the script
-echo "number of arguments:" $#
+echo "Number of arguments:" $#
 #prints 2nd and 4th argument
 echo "2nd argument: $2"
 echo "4th argument: $4"
-
+#print the exit code of -eq operation on 1st and 2nd arguments
 var1=$1
 var2=$2
 [[ ${var1} -eq ${var2} ]]
