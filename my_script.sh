0619b75d52e2fbbd68db779a4f9b09eece8d276e
Yaroslav <yaroslav.garashchenko@gmail.com>
2022-09-07 11:10:28 +0300

    Create my_script.sh

---
+++ my_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Task #2 for topic of if statement
+#takes any number of arguments
+#if there are less than 2 arguments, output the values of these arguments
+#if there are more than 2 but less than 4 arguments, output only the last argument
+#else output "Invalid number of arguments" message
+if (( $# < 2 )); then
+   echo $*
+elif (( $# > 2 && $# < 4 )); then
+   echo $3
+else
+   echo "Invalid number of arguments"
+fi
